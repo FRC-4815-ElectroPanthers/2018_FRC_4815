c0ac7b22b62ef71fb1457efcc3832ad5d8c9e905
clhsr <clhsr@DESKTOP-U7BMSH3>
2018-02-20 22:04:35 -0500

    Comment Out Pixy Library

--- src/PixyCamera/PixyI2C.cpp
+++ src/PixyCamera/PixyI2C.cpp
@@ -1,274 +1,274 @@
-/*
- * PixyI2C.cpp
- *
- *  Created on: Feb 14, 2018
- *      Author: 4815
- */
-#include "PixyI2C.h"
-#include "Timer.h"
-
-PixyI2C::PixyI2C(uint8_t address, I2C::Port port): Wire(address, port){
-	m_skipStart = false;
-	m_blockType = NORMAL_BLOCK;
-	m_blockCount = 0;
-	m_blockArraySize = PIXY_INITIAL_ARRAYSIZE;
-	//blocks = (Block *)malloc(sizeof(Block)*blockArraySize);
-
-	m_freed = false;
-	m_GetBlocks = std::thread(&PixyI2C::GetBlocks(), this);
-}
-
-PixyI2C::~PixyI2C(){
-	m_freed = true;
-
-	if(m_GetBlocks.joinable()){
-		m_GetBlocks.join();
-		//free(blocks);
-	}
-}
-
-uint16_t PixyI2C::getWord(){
-	uint8_t c[2];
-	Wire.ReadOnly(2, c);
-	uint16_t w = (c[1] << 8) + c[0];
-
-	return w;
-}
-
-uint8_t PixyI2C::getByte(){
-	uint8_t *c;
-	Wire.ReadOnly(1, c);
-
-	return *c;
-}
-
-int8_t PixyI2C::send(uint8_t *data, uint8_t len){
-	Wire.WriteBulk(data, len);
-
-	return len;
-}
-
-bool PixyI2C::GetStart(){
-	uint16_t w, lastw;
-
-	lastw = 0xffff;
-
-	while(true){
-		w = getWord();
-
-		if (w==0 && lastw==0){
-			Timer* delay = new Timer();
-			delay->Start();
-
-			while (!delay->HasPeriodPassed(0.00005)) {
-				// Wait 50 microseconds
-			}
-
-			return false;
-
-		}else if (w==PIXY_START_WORD && lastw==PIXY_START_WORD){
-			std::lock_guard lock(m_mutex);
-
-			m_blockType = NORMAL_BLOCK;
-			return true;
-
-		}else if (w==PIXY_START_WORD_CC && lastw==PIXY_START_WORD){
-			std::lock_guard lock(m_mutex);
-
-			m_blockType = CC_BLOCK;
-			return true;
-
-		}else if (w==PIXY_START_WORDX){
-			printf("reorder");
-			getByte(); // resync
-		}
-
-		lastw = w;
-	}
-
-}
-
-/*
-void PixyI2C::Resize(){
-	std::lock_guard lock(m_mutex);
-
-	blockArraySize += PIXY_INITIAL_ARRAYSIZE;
-	blocks = (Block *)realloc(blocks, sizeof(Block)*blockArraySize);
-}
-*/
-
-void PixyI2C::GetBlocks(uint16_t maxBlocks){
-	uint8_t i;
-	uint16_t w, checksum, sum, buffer[6];
-	Block temp;
-	//Block *block;
-
-	BlockType blockType;
-	uint16_t blockCount;
-	std::vector<Block> block;
-
-	{
-		std::lock_guard lock(m_mutex);
-		blockType = m_blockType;
-	}
-
-	while(!m_freed){
-		blockCount = 0;
-		if(!block.empty()){
-			block.clear();
-		}
-
-		if (!m_skipStart){
-			if (GetStart()==false){
-				std::lock_guard lock(m_mutex);
-
-				m_blockCount = blockCount;
-				continue; //return;
-			}
-		}else{
-			m_skipStart = false;
-		}
-
-		for(blockCount=0; blockCount<maxBlocks && blockCount<PIXY_MAXIMUM_ARRAYSIZE;){
-
-			checksum = getWord();
-			if (checksum==PIXY_START_WORD){
-				// we've reached the beginning of the next frame
-				m_skipStart = true;
-				blockType = NORMAL_BLOCK;
-				break;
-			}else if (checksum==PIXY_START_WORD_CC){
-				m_skipStart = true;
-				blockType = CC_BLOCK;
-				break;
-			}else if (checksum==0){
-				break;
-			}
-
-			/*
-			if (m_blockCount>m_blockArraySize){
-				m_Resize();
-			}
-
-
-			block = blocks + blockCount;
-			*/
-
-			for (i=0, sum=0; i < 6; i++){
-				if (blockType==NORMAL_BLOCK && i==5){ //skip
-					buffer[i] = 0;
-					break;
-				}
-
-				buffer[i] = getWord();
-				sum += buffer[i];
-				//*((uint16_t *)block + i) = w;
-			}
-			temp.signature = buffer[0];
-			temp.x = buffer[1];
-			temp.y = buffer[2];
-			temp.width= buffer[3];
-			temp.height = buffer[4];
-			temp.angle = buffer[5];
-
-			block.push_back(temp);
-
-			if (checksum==sum){
-				blockCount++;
-			}else{
-				printf("CheckSum Error");
-			}
-
-			w = getWord();
-			if (w==PIXY_START_WORD){
-				blockType = NORMAL_BLOCK;
-			}else if (w==PIXY_START_WORD_CC){
-				blockType = CC_BLOCK;
-			}else{
-				break;
-			}
-		}
-
-		{
-			std::lock_guard lock(m_mutex);
-
-			m_blockType = blockType;
-			m_blockCount = blockCount;
-			m_blocks = block;
-		}
-	}
-
-
-}
-
-uint16_t PixyI2C::GetBlockCount(){
-	std::lock_guard lock(m_mutex);
-	return m_blockCount;
-}
-
-uint16_t PixyI2C::GetBlockSignature(int block){
-	std::lock_guard lock(m_mutex);
-	return m_blocks[block].signature;
-}
-
-uint16_t PixyI2C::GetBlockX(int block){
-	std::lock_guard lock(m_mutex);
-	return m_blocks[block].x;
-}
-
-uint16_t PixyI2C::GetBlockY(int block){
-	std::lock_guard lock(m_mutex);
-	return m_blocks[block].y;
-}
-
-uint16_t PixyI2C::GetBlockWidth(int block){
-	std::lock_guard lock(m_mutex);
-	return m_blocks[block].width;
-}
-
-uint16_t PixyI2C::GetBlockHeight(int block){
-	std::lock_guard lock(m_mutex);
-	return m_blocks[block].height;
-}
-
-uint16_t PixyI2C::GetBlockAngle(int block){
-	std::lock_guard lock(m_mutex);
-	return m_blocks[block].angle;
-}
-
-int8_t PixyI2C::SetServos(uint16_t s0, uint16_t s1){
-	uint8_t outBuf[6];
-
-	outBuf[0] = 0x00;
-	outBuf[1] = 0xff;
-	*(uint16_t *)(outBuf + 2) = s0;
-	*(uint16_t *)(outBuf + 4) = s1;
-
-	return send(outBuf, 6);
-}
-
-int8_t PixyI2C::SetBrightness(uint8_t brightness){
-	uint8_t outBuf[3];
-
-	outBuf[0] = 0x00;
-	outBuf[1] = 0xfe;
-	outBuf[2] = brightness;
-
-	return send(outBuf, 3);
-}
-
-int8_t PixyI2C::SetLED(uint8_t r, uint8_t g, uint8_t b){
-	uint8_t outBuf[5];
-
-	outBuf[0] = 0x00;
-	outBuf[1] = 0xfd;
-	outBuf[2] = r;
-	outBuf[3] = g;
-	outBuf[4] = b;
-
-	return send(outBuf, 5);
-}
-
-
-
-
+///*
+// * PixyI2C.cpp
+// *
+// *  Created on: Feb 14, 2018
+// *      Author: 4815
+// */
+//#include "PixyI2C.h"
+//#include "Timer.h"
+//
+//PixyI2C::PixyI2C(uint8_t address, I2C::Port port): Wire(address, port){
+//	m_skipStart = false;
+//	m_blockType = NORMAL_BLOCK;
+//	m_blockCount = 0;
+//	m_blockArraySize = PIXY_INITIAL_ARRAYSIZE;
+//	//blocks = (Block *)malloc(sizeof(Block)*blockArraySize);
+//
+//	m_freed = false;
+//	m_GetBlocks = std::thread(&PixyI2C::GetBlocks(), this);
+//}
+//
+//PixyI2C::~PixyI2C(){
+//	m_freed = true;
+//
+//	if(m_GetBlocks.joinable()){
+//		m_GetBlocks.join();
+//		//free(blocks);
+//	}
+//}
+//
+//uint16_t PixyI2C::getWord(){
+//	uint8_t c[2];
+//	Wire.ReadOnly(2, c);
+//	uint16_t w = (c[1] << 8) + c[0];
+//
+//	return w;
+//}
+//
+//uint8_t PixyI2C::getByte(){
+//	uint8_t *c;
+//	Wire.ReadOnly(1, c);
+//
+//	return *c;
+//}
+//
+//int8_t PixyI2C::send(uint8_t *data, uint8_t len){
+//	Wire.WriteBulk(data, len);
+//
+//	return len;
+//}
+//
+//bool PixyI2C::GetStart(){
+//	uint16_t w, lastw;
+//
+//	lastw = 0xffff;
+//
+//	while(true){
+//		w = getWord();
+//
+//		if (w==0 && lastw==0){
+//			Timer* delay = new Timer();
+//			delay->Start();
+//
+//			while (!delay->HasPeriodPassed(0.00005)) {
+//				// Wait 50 microseconds
+//			}
+//
+//			return false;
+//
+//		}else if (w==PIXY_START_WORD && lastw==PIXY_START_WORD){
+//			std::lock_guard<std::mutex> lock(m_mutex);
+//
+//			m_blockType = NORMAL_BLOCK;
+//			return true;
+//
+//		}else if (w==PIXY_START_WORD_CC && lastw==PIXY_START_WORD){
+//			std::lock_guard<std::mutex> lock(m_mutex);
+//
+//			m_blockType = CC_BLOCK;
+//			return true;
+//
+//		}else if (w==PIXY_START_WORDX){
+//			printf("reorder");
+//			getByte(); // resync
+//		}
+//
+//		lastw = w;
+//	}
+//
+//}
+//
+///*
+//void PixyI2C::Resize(){
+//	std::lock_guard<std::mutex> lock(m_mutex);
+//
+//	blockArraySize += PIXY_INITIAL_ARRAYSIZE;
+//	blocks = (Block *)realloc(blocks, sizeof(Block)*blockArraySize);
+//}
+//*/
+//
+//void PixyI2C::GetBlocks(uint16_t maxBlocks){
+//	uint8_t i;
+//	uint16_t w, checksum, sum, buffer[6];
+//	Block temp;
+//	//Block *block;
+//
+//	BlockType blockType;
+//	uint16_t blockCount;
+//	std::vector<Block> block;
+//
+//	{
+//		std::lock_guard<std::mutex> lock(m_mutex);
+//		blockType = m_blockType;
+//	}
+//
+//	while(!m_freed){
+//		blockCount = 0;
+//		if(!block.empty()){
+//			block.clear();
+//		}
+//
+//		if (!m_skipStart){
+//			if (GetStart()==false){
+//				std::lock_guard<std::mutex> lock(m_mutex);
+//
+//				m_blockCount = blockCount;
+//				continue; //return;
+//			}
+//		}else{
+//			m_skipStart = false;
+//		}
+//
+//		for(blockCount=0; blockCount<maxBlocks && blockCount<PIXY_MAXIMUM_ARRAYSIZE;){
+//
+//			checksum = getWord();
+//			if (checksum==PIXY_START_WORD){
+//				// we've reached the beginning of the next frame
+//				m_skipStart = true;
+//				blockType = NORMAL_BLOCK;
+//				break;
+//			}else if (checksum==PIXY_START_WORD_CC){
+//				m_skipStart = true;
+//				blockType = CC_BLOCK;
+//				break;
+//			}else if (checksum==0){
+//				break;
+//			}
+//
+//			/*
+//			if (m_blockCount>m_blockArraySize){
+//				m_Resize();
+//			}
+//
+//
+//			block = blocks + blockCount;
+//			*/
+//
+//			for (i=0, sum=0; i < 6; i++){
+//				if (blockType==NORMAL_BLOCK && i==5){ //skip
+//					buffer[i] = 0;
+//					break;
+//				}
+//
+//				buffer[i] = getWord();
+//				sum += buffer[i];
+//				//*((uint16_t *)block + i) = w;
+//			}
+//			temp.signature = buffer[0];
+//			temp.x = buffer[1];
+//			temp.y = buffer[2];
+//			temp.width= buffer[3];
+//			temp.height = buffer[4];
+//			temp.angle = buffer[5];
+//
+//			block.push_back(temp);
+//
+//			if (checksum==sum){
+//				blockCount++;
+//			}else{
+//				printf("CheckSum Error");
+//			}
+//
+//			w = getWord();
+//			if (w==PIXY_START_WORD){
+//				blockType = NORMAL_BLOCK;
+//			}else if (w==PIXY_START_WORD_CC){
+//				blockType = CC_BLOCK;
+//			}else{
+//				break;
+//			}
+//		}
+//
+//		{
+//			std::lock_guard<std::mutex> lock(m_mutex);
+//
+//			m_blockType = blockType;
+//			m_blockCount = blockCount;
+//			m_blocks = block;
+//		}
+//	}
+//
+//
+//}
+//
+//uint16_t PixyI2C::GetBlockCount(){
+//	std::lock_guard<std::mutex> lock(m_mutex);
+//	return m_blockCount;
+//}
+//
+//uint16_t PixyI2C::GetBlockSignature(int block){
+//	std::lock_guard<std::mutex> lock(m_mutex);
+//	return m_blocks[block].signature;
+//}
+//
+//uint16_t PixyI2C::GetBlockX(int block){
+//	std::lock_guard<std::mutex> lock(m_mutex);
+//	return m_blocks[block].x;
+//}
+//
+//uint16_t PixyI2C::GetBlockY(int block){
+//	std::lock_guard<std::mutex> lock(m_mutex);
+//	return m_blocks[block].y;
+//}
+//
+//uint16_t PixyI2C::GetBlockWidth(int block){
+//	std::lock_guard<std::mutex> lock(m_mutex);
+//	return m_blocks[block].width;
+//}
+//
+//uint16_t PixyI2C::GetBlockHeight(int block){
+//	std::lock_guard<std::mutex> lock(m_mutex);
+//	return m_blocks[block].height;
+//}
+//
+//uint16_t PixyI2C::GetBlockAngle(int block){
+//	std::lock_guard<std::mutex> lock(m_mutex);
+//	return m_blocks[block].angle;
+//}
+//
+//int8_t PixyI2C::SetServos(uint16_t s0, uint16_t s1){
+//	uint8_t outBuf[6];
+//
+//	outBuf[0] = 0x00;
+//	outBuf[1] = 0xff;
+//	*(uint16_t *)(outBuf + 2) = s0;
+//	*(uint16_t *)(outBuf + 4) = s1;
+//
+//	return send(outBuf, 6);
+//}
+//
+//int8_t PixyI2C::SetBrightness(uint8_t brightness){
+//	uint8_t outBuf[3];
+//
+//	outBuf[0] = 0x00;
+//	outBuf[1] = 0xfe;
+//	outBuf[2] = brightness;
+//
+//	return send(outBuf, 3);
+//}
+//
+//int8_t PixyI2C::SetLED(uint8_t r, uint8_t g, uint8_t b){
+//	uint8_t outBuf[5];
+//
+//	outBuf[0] = 0x00;
+//	outBuf[1] = 0xfd;
+//	outBuf[2] = r;
+//	outBuf[3] = g;
+//	outBuf[4] = b;
+//
+//	return send(outBuf, 5);
+//}
+//
+//
+//
+//
--- src/PixyCamera/PixyI2C.h
+++ src/PixyCamera/PixyI2C.h
@@ -1,144 +1,144 @@
+////
+//// begin license header
+////
+//// This file is part of Pixy CMUcam5 or "Pixy" for short
+////
+//// All Pixy source code is provided under the terms of the
+//// GNU General Public License v2 (http://www.gnu.org/licenses/gpl-2.0.html).
+//// Those wishing to use Pixy source code, software and/or
+//// technologies under different licensing terms should contact us at
+//// cmucam@cs.cmu.edu. Such licensing terms are available for
+//// all portions of the Pixy codebase presented here.
+////
+//// end license header
+////
+//// Note, the PixyI2C class takes two optional arguments, the first being the I2C address
+//// of the Pixy you want to talk to and the second being the port on the RoboRIO you want to use (Onboard or MXP).
+//// The default address and port are 0x54 and kOnboard respectively.
+//// So, for example, if you wished to talk to Pixy at I2C address 0x55 and using the MXP port, declare like this:
+////
+//// PixyI2C *Pixy = new PixyI2C(0x55, I2C::Port::kMXP);
+////
 //
-// begin license header
+//#ifndef _PIXYI2C_H
+//#define _PIXYI2C_H
 //
-// This file is part of Pixy CMUcam5 or "Pixy" for short
+//#include <atomic>
+//#include <cstdint>
+//#include <memory>
+//#include <thread>
+//#include <mutex>
+//#include <condition_variable>
+//#include <vector>
 //
-// All Pixy source code is provided under the terms of the
-// GNU General Public License v2 (http://www.gnu.org/licenses/gpl-2.0.html).
-// Those wishing to use Pixy source code, software and/or
-// technologies under different licensing terms should contact us at
-// cmucam@cs.cmu.edu. Such licensing terms are available for
-// all portions of the Pixy codebase presented here.
+//#include "I2C.h"
+//#include "SensorBase.h"
+////#include <support/mutex.h>
+////#include <support/condition_variable.h>
 //
-// end license header
+//// Communication/misc parameters
+//#define PIXY_INITIAL_ARRAYSIZE      30
+//#define PIXY_MAXIMUM_ARRAYSIZE      130
+//#define PIXY_START_WORD             0xaa55
+//#define PIXY_START_WORD_CC          0xaa56
+//#define PIXY_START_WORDX            0x55aa
+//#define PIXY_MAX_SIGNATURE          7
+//#define PIXY_DEFAULT_ARGVAL         0xffff
 //
-// Note, the PixyI2C class takes two optional arguments, the first being the I2C address 
-// of the Pixy you want to talk to and the second being the port on the RoboRIO you want to use (Onboard or MXP).
-// The default address and port are 0x54 and kOnboard respectively.
-// So, for example, if you wished to talk to Pixy at I2C address 0x55 and using the MXP port, declare like this:
+//// Pixy x-y position values
+//#define PIXY_MIN_X                  0L
+//#define PIXY_MAX_X                  319L
+//#define PIXY_MIN_Y                  0L
+//#define PIXY_MAX_Y                  199L
 //
-// PixyI2C *Pixy = new PixyI2C(0x55, I2C::Port::kMXP);
+//// RC-servo values
+//#define PIXY_RCS_MIN_POS            0L
+//#define PIXY_RCS_MAX_POS            1000L
+//#define PIXY_RCS_CENTER_POS         ((PIXY_RCS_MAX_POS-PIXY_RCS_MIN_POS)/2)
+//#define PIXY_FOV					75
 //
-
-#ifndef _PIXYI2C_H
-#define _PIXYI2C_H
-
-#include <atomic>
-#include <cstdint>
-#include <memory>
-#include <thread>
-#include <mutex>
-#include <condition_variable>
-#include <vector>
-
-#include "I2C.h"
-#include "SensorBase.h"
-//#include <support/mutex.h>
-//#include <support/condition_variable.h>
-
-// Communication/misc parameters
-#define PIXY_INITIAL_ARRAYSIZE      30
-#define PIXY_MAXIMUM_ARRAYSIZE      130
-#define PIXY_START_WORD             0xaa55
-#define PIXY_START_WORD_CC          0xaa56
-#define PIXY_START_WORDX            0x55aa
-#define PIXY_MAX_SIGNATURE          7
-#define PIXY_DEFAULT_ARGVAL         0xffff
-
-// Pixy x-y position values
-#define PIXY_MIN_X                  0L
-#define PIXY_MAX_X                  319L
-#define PIXY_MIN_Y                  0L
-#define PIXY_MAX_Y                  199L
-
-// RC-servo values
-#define PIXY_RCS_MIN_POS            0L
-#define PIXY_RCS_MAX_POS            1000L
-#define PIXY_RCS_CENTER_POS         ((PIXY_RCS_MAX_POS-PIXY_RCS_MIN_POS)/2)
-#define PIXY_FOV					75
-
-#define PIXY_I2C_DEFAULT_ADDR  0x54
-#define PIXY_I2C_DEFAULT_PORT  I2C::Port::kOnboard
-
-class PixyI2C{
-public:
-	PixyI2C(uint8_t address=PIXY_I2C_DEFAULT_ADDR, I2C::Port port=PIXY_I2C_DEFAULT_PORT);
-	~PixyI2C();
-
-	uint16_t GetBlockCount();
-	uint16_t GetBlockSignature(int = 0);
-	uint16_t GetBlockX(int = 0);
-	uint16_t GetBlockY(int = 0);
-	uint16_t GetBlockWidth(int = 0);
-	uint16_t GetBlockHeight(int = 0);
-	uint16_t GetBlockAngle(int = 0);
-
-	int8_t SetServos(uint16_t s0, uint16_t s1);
-	int8_t SetBrightness(uint8_t brightness);
-	int8_t SetLED(uint8_t r, uint8_t g, uint8_t b);
-
-private:
-	enum BlockType{
-	  NORMAL_BLOCK,
-	  CC_BLOCK
-	};
-
-	struct Block {
-		// print block structure!
-		void print(){
-			int i, j;
-			char buf[128], sig[6], d;
-			bool flag;
-
-			if (signature>PIXY_MAX_SIGNATURE){ // color code! (CC)
-				// convert signature number to an octal string
-				for (i=12, j=0, flag=false; i>=0; i-=3){
-					d = (signature>>i)&0x07;
-
-					if (d>0 && !flag){
-						flag = true;
-					}
-
-					if (flag){
-						sig[j++] = d + '0';
-					}
-				}
-
-				sig[j] = '\0';
-				sprintf(buf, "CC block! sig: %s (%d decimal) x: %d y: %d width: %d height: %d angle %d\n", sig, signature, x, y, width, height, angle);
-			}else{ // regular block.  Note, angle is always zero, so no need to print
-				sprintf(buf, "sig: %d x: %d y: %d width: %d height: %d\n", signature, x, y, width, height);
-			}
-			printf(buf);
-		}
-
-		uint16_t signature;
-		uint16_t x;
-		uint16_t y;
-		uint16_t width;
-		uint16_t height;
-		uint16_t angle;
-	};
-
-	I2C Wire;
-	bool  m_skipStart;
-	BlockType m_blockType;
-	uint16_t m_blockCount;
-	uint16_t m_blockArraySize;
-	//Block *m_blocks;
-	std::vector<Block> m_blocks;
-
-	uint16_t getWord();
-	uint8_t getByte();
-	int8_t send(uint8_t *data, uint8_t len);
-
-	void GetBlocks(uint16_t maxBlocks=1000);
-	bool GetStart();
-	//void Resize();
-
-	std::atomic_bool m_freed;
-	mutable std::mutex m_mutex;
-	std::thread m_GetBlocks;
-};
-
-#endif
+//#define PIXY_I2C_DEFAULT_ADDR  0x54
+//#define PIXY_I2C_DEFAULT_PORT  I2C::Port::kOnboard
+//
+//class PixyI2C{
+//public:
+//	PixyI2C(uint8_t address=PIXY_I2C_DEFAULT_ADDR, I2C::Port port=PIXY_I2C_DEFAULT_PORT);
+//	~PixyI2C();
+//
+//	uint16_t GetBlockCount();
+//	uint16_t GetBlockSignature(int = 0);
+//	uint16_t GetBlockX(int = 0);
+//	uint16_t GetBlockY(int = 0);
+//	uint16_t GetBlockWidth(int = 0);
+//	uint16_t GetBlockHeight(int = 0);
+//	uint16_t GetBlockAngle(int = 0);
+//
+//	int8_t SetServos(uint16_t s0, uint16_t s1);
+//	int8_t SetBrightness(uint8_t brightness);
+//	int8_t SetLED(uint8_t r, uint8_t g, uint8_t b);
+//
+//private:
+//	enum BlockType{
+//	  NORMAL_BLOCK,
+//	  CC_BLOCK
+//	};
+//
+//	struct Block {
+//		// print block structure!
+//		void print(){
+//			int i, j;
+//			char buf[128], sig[6], d;
+//			bool flag;
+//
+//			if (signature>PIXY_MAX_SIGNATURE){ // color code! (CC)
+//				// convert signature number to an octal string
+//				for (i=12, j=0, flag=false; i>=0; i-=3){
+//					d = (signature>>i)&0x07;
+//
+//					if (d>0 && !flag){
+//						flag = true;
+//					}
+//
+//					if (flag){
+//						sig[j++] = d + '0';
+//					}
+//				}
+//
+//				sig[j] = '\0';
+//				sprintf(buf, "CC block! sig: %s (%d decimal) x: %d y: %d width: %d height: %d angle %d\n", sig, signature, x, y, width, height, angle);
+//			}else{ // regular block.  Note, angle is always zero, so no need to print
+//				sprintf(buf, "sig: %d x: %d y: %d width: %d height: %d\n", signature, x, y, width, height);
+//			}
+//			printf(buf);
+//		}
+//
+//		uint16_t signature;
+//		uint16_t x;
+//		uint16_t y;
+//		uint16_t width;
+//		uint16_t height;
+//		uint16_t angle;
+//	};
+//
+//	I2C Wire;
+//	bool  m_skipStart;
+//	BlockType m_blockType;
+//	uint16_t m_blockCount;
+//	uint16_t m_blockArraySize;
+//	//Block *m_blocks;
+//	std::vector<Block> m_blocks;
+//
+//	uint16_t getWord();
+//	uint8_t getByte();
+//	int8_t send(uint8_t *data, uint8_t len);
+//
+//	void GetBlocks(uint16_t maxBlocks=1000);
+//	bool GetStart();
+//	//void Resize();
+//
+//	std::atomic_bool m_freed;
+//	mutable std::mutex m_mutex;
+//	std::thread m_GetBlocks;
+//};
+//
+//#endif
